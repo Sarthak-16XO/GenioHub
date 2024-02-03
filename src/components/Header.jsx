39c39cb04f303b348c0eadd7cd05c656016bd5bb
Sarthak <sarthakxo16@gmail.com>
2024-02-03 21:50:09 +0530

    ui changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,13 +24,13 @@ function Header() {
   return (
 
     <nav className="bg-slate-900 border-gray-200">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="w-full flex flex-row   justify-between md:mx-auto p-4">
         <Link to='/' className="flex items-center">
-          <img src={logo} className="h-10 mr-3 rounded-md" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold text-white whitespace-nowrap">GenioHub</span>
+          <img src={logo} className="h-10 mr-3 rounded-md" alt="Logo" />
+          <p className="self-center text-2xl font-semibold text-white whitespace-nowrap">GenioHub</p>
         </Link>
 
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div className="w-full md:w-auto" >
           <nav className="">
             <SignedOut>
               <ul className="flex flex-row gap-10">
@@ -40,12 +40,10 @@ function Header() {
             </SignedOut>
 
             <SignedIn>
-              <div className="flex flex-row items-center gap-8">
+              <div className="flex flex-row items-center gap-4">
                 <Link to="/dashboard" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" >Get Started</Link>
                 <UserButton afterSignOutUrl="/" />
-
               </div>
-
             </SignedIn>
           </nav>
         </div>
